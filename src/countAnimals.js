108f4f17dd85c4ac38a82ce2fc56d26bda4a11af
Andre Rousselet <andre098@hotmail.com>
2022-03-16 20:01:28 -0300

    Requisito 5: retorna o objeto com especies e contagem

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,26 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+// console.log(species);
+
 function countAnimals(animal) {
-  // seu código aqui
+  return species.reduce((objOfSpecies, currentSpecie) => {
+    objOfSpecies[currentSpecie.name] = currentSpecie.residents.length;
+    return objOfSpecies;
+  }, {});
 }
+console.log(countAnimals());
 
 module.exports = countAnimals;
+
+// const players = [
+//   { fullName: 'Adriano Imperador', email: 'didico@futebol.br' },
+//   { fullName: 'Ronaldinho Gaúcho', email: 'bruxo@futebol.br' },
+//   { fullName: 'Ronaldo Fenômeno', email: 'cortedocascao@futebol.br' },
+//   { fullName: 'Marta Vieira da Silva', email: 'rainhamarta@futebol.br' },
+// ];
+// const playerObj = players.reduce((accumulator, currentValue) => {
+//   accumulator[currentValue.fullName] = currentValue.email;
+//   return accumulator;
+// }, {});
+// console.log(playerObj);
