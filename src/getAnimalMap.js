783043cd4df086921781c8e3865b179cbd58a2df
Andre Rousselet <andre098@hotmail.com>
2022-03-16 20:50:06 -0300

    Requisito 7: inicia fazendo função com parametro indefinido

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,17 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+
 function getAnimalMap(options) {
-  // seu código aqui
+  return species.reduce((acc, currSpecie) => {
+    if (!acc[currSpecie.location]) {
+      acc[currSpecie.location] = [];
+    }
+    const animal = species.find((specie) => specie.name === currSpecie.name).name;
+    acc[currSpecie.location].push(animal);
+    return acc;
+  }, {});
 }
 
+console.log(getAnimalMap());
 module.exports = getAnimalMap;
