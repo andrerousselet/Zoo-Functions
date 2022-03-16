ca1a3ac8d0f5ffeda651975041aae5e148528089
Andre Rousselet <andre098@hotmail.com>
2022-03-16 16:25:01 -0300

    Requisito 2: usa obj destruturing, find e every

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,9 @@
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  const { species } = data;
+  const anml = species.find((specie) => specie.name === animal);
+  return anml.residents.every((resident) => resident.age >= age);
 }
 
 module.exports = getAnimalsOlderThan;
