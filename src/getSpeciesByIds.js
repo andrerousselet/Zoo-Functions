14fd77bc9dedb56d3a6d3d50fa717b956513038c
Andre Rousselet <andre098@hotmail.com>
2022-03-16 17:19:17 -0300

    Requisito 1: usa map para percorrer os parametros e find para cada parametro

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,13 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  //  seu código aqui
+const { species } = data;
+// console.log(species);
+
+function getSpeciesByIds(...ids) {
+  if (ids.length === 0) {
+    return [];
+  }
+  return ids.map((id) => species.find((specie) => specie.id === id));
 }
 
 module.exports = getSpeciesByIds;
