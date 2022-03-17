06825c62ae13fcb3c4238d7e0514e48023cea8c2
Andre Rousselet <andre098@hotmail.com>
2022-03-17 12:07:04 -0300

    Requisito 2: adiciona comentários e altera verificação de parâmetro indefinido usando 'not'

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -2,14 +2,15 @@ const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
   const { employees } = data;
-  const emptyObj = {};
-  if (employeeName === undefined) {
-    return emptyObj;
-  }
+  // const emptyObj = {};
+  // if (employeeName === undefined) {
+  //   return emptyObj;
+  // }
+  if (!employeeName) return {};
+  // Usa find para retornar o objeto do funcionário que tiver o nome
+  // OU o sobrenome iguais ao parâmetro.
   return employees.find((employee) =>
     (employee.firstName === employeeName || employee.lastName === employeeName));
 }
-
-console.log(getEmployeeByName());
 
 module.exports = getEmployeeByName;
