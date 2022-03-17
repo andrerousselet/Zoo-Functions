c7f1601a7792ccab12615887d4e9382e9d31132f
Andre Rousselet <andre098@hotmail.com>
2022-03-17 19:13:41 -0300

    Requisito 9: usa find, sort e Object.values

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,15 @@
 const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
+const { employees } = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  // Busca o funcionário de acordo com id passado
+  const worker = employees.find((employee) => id === employee.id);
+  // Busca primeiro animal de responsabilidade do funcionário achado anteriormente
+  const animal = species.find((specie) => specie.id === worker.responsibleFor[0]);
+  // Usa método sort() nos residents do animal para organizar por idade decrescente (mais velho na posição [0])
+  const sorted = animal.residents.sort((a, b) => b.age - a.age);
+  return Object.values(sorted[0]);
 }
 
 module.exports = getOldestFromFirstSpecies;
