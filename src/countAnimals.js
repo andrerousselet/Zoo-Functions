1c43e20b1d5ec852ef3314424a5d03b010781bae
Andre Rousselet <andre098@hotmail.com>
2022-03-17 11:36:35 -0300

    Requisito 5: cria função usando reduce, find, filter e verificações

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,33 +1,24 @@
 const data = require('../data/zoo_data');
 
 const { species } = data;
-// console.log(species);
 
 function countAnimals(animal) {
+  // Se animal não for definido, cria objeto com reduce, adicionando a cada iteração,
+  // o nome da specie (currentValue) como chave e a quantidade de residents como respectivo valor.
   if (animal === undefined) {
-    return species.reduce((acc, currentSpecie) => {
-      const objOfSpecies = acc;
-      if (!objOfSpecies[currentSpecie.name]) {
-        objOfSpecies[currentSpecie.name] = 0;
-      }
-      objOfSpecies[currentSpecie.name] = currentSpecie.residents.length;
-      return objOfSpecies;
+    return species.reduce((acc, currentSpecie) => { // com o nome acc, não tem problema, mas com qualquer outro nome, o linter acusa erro...por que???
+      acc[currentSpecie.name] = currentSpecie.residents.length;
+      return acc;
     }, {});
   }
-  // return species.find(() => {}, 0);
+  // Cria constante com os residents de cada specie que tiver o mesmo nome do parâmetro. Depois,
+  // verfica se o parâmetro possui a chave sex: se tiver, filtra e retorna a quantidade, se não,
+  // retorna apenas a quantidade de residentes.
+  const residentsOfSpecie = species.find((specie) => specie.name === animal.specie).residents;
+  if (Object.keys(animal).includes('sex')) {
+    return residentsOfSpecie.filter((resident) => resident.sex === animal.sex).length;
+  }
+  return residentsOfSpecie.length;
 }
-console.log(countAnimals());
 
 module.exports = countAnimals;
-
-// const players = [
-//   { fullName: 'Adriano Imperador', email: 'didico@futebol.br' },
-//   { fullName: 'Ronaldinho Gaúcho', email: 'bruxo@futebol.br' },
-//   { fullName: 'Ronaldo Fenômeno', email: 'cortedocascao@futebol.br' },
-//   { fullName: 'Marta Vieira da Silva', email: 'rainhamarta@futebol.br' },
-// ];
-// const playerObj = players.reduce((accumulator, currentValue) => {
-//   accumulator[currentValue.fullName] = currentValue.email;
-//   return accumulator;
-// }, {});
-// console.log(playerObj);
