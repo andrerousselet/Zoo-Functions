96a295d24f6c429ebbd8b6eeea878f63f5331f09
Andre Rousselet <andre098@hotmail.com>
2022-03-17 12:00:33 -0300

    Requisito 2: adiciona comentários

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,8 +1,12 @@
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
+  // Desestrutura a chave species do objeto data.
   const { species } = data;
+  // Cria constante que armazena a especie encontrada caso o parâmetro seja igual ao specie.name.
   const anml = species.find((specie) => specie.name === animal);
+  // Acessa a chave residents da const anterior e retorna true se todos
+  // os residentes tiverem idade maior que o parâmetro passado.
   return anml.residents.every((resident) => resident.age >= age);
 }
 
