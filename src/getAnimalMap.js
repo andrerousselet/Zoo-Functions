3cb8e91da9c59bf3ae4226ce26fa040bf37dc1f5
Andre Rousselet <andre098@hotmail.com>
2022-03-22 15:12:36 -0300

    Requisito 7: cumprer o at[e o sexto item, faltando o ultimo

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,17 +1,57 @@
 const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
-const { species } = data;
-
-function getAnimalMap(options) {
+function noParam() {
   return species.reduce((acc, currSpecie) => {
-    if (!acc[currSpecie.location]) {
-      acc[currSpecie.location] = [];
-    }
-    const animal = species.find((specie) => specie.name === currSpecie.name).name;
-    acc[currSpecie.location].push(animal);
+    if (!acc[currSpecie.location]) acc[currSpecie.location] = [];
+    const animal = species.find((specie) => specie.name === currSpecie.name);
+    acc[currSpecie.location].push(animal.name);
     return acc;
   }, {});
 }
 
-console.log(getAnimalMap());
+function includesNameTrue() {
+  return species.reduce((acc, currSpecie) => {
+    if (!acc[currSpecie.location]) acc[currSpecie.location] = [];
+    const animal = species.find((specie) => specie.name === currSpecie.name);
+    const residentsNames = animal.residents.map((resident) => resident.name);
+    const obj = { [animal.name]: residentsNames };
+    acc[currSpecie.location].push(obj);
+    return acc;
+  }, {});
+}
+
+function sortedTrue() {
+  return species.reduce((acc, currSpecie) => {
+    if (!acc[currSpecie.location]) acc[currSpecie.location] = [];
+    const animal = species.find((specie) => specie.name === currSpecie.name);
+    const residentsNames = animal.residents.map((resident) => resident.name).sort();
+    const obj = { [animal.name]: residentsNames };
+    acc[currSpecie.location].push(obj);
+    return acc;
+  }, {});
+}
+
+function sexFilter(sex) {
+  return species.reduce((acc, currSpecie) => {
+    if (!acc[currSpecie.location]) acc[currSpecie.location] = [];
+    const animal = species.find((specie) => specie.name === currSpecie.name);
+    const residentsNames = animal.residents
+      .filter((resident) => resident.sex === sex)
+      .map((element) => element.name);
+    const obj = { [animal.name]: residentsNames };
+    acc[currSpecie.location].push(obj);
+    return acc;
+  }, {});
+}
+
+function getAnimalMap(options) {
+  if (!options || !options.includeNames) return noParam();
+  if (options.sorted) return sortedTrue();
+  if (options.sex) return sexFilter(options.sex);
+
+  return includesNameTrue();
+}
+console.log(getAnimalMap({ includeNames: true }));
+
 module.exports = getAnimalMap;
