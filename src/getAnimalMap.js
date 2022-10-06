6f8d822e74d848f7c39af296da5bf671b6c42f09
Andre Rousselet <andre098@hotmail.com>
2022-10-06 19:00:21 -0300

    feat(requisito 7): instead of sortTrue function, sort names in both sexFilter and includeNamesTrue
    if options.sorted is true, then sort residents name in each function...

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -10,35 +10,37 @@ function noParam() {
   }, {});
 }
 
-function includesNameTrue() {
+function includesNameTrue(options) {
   return species.reduce((acc, currSpecie) => {
     if (!acc[currSpecie.location]) acc[currSpecie.location] = [];
     const animal = species.find((specie) => specie.name === currSpecie.name);
     const residentsNames = animal.residents.map((resident) => resident.name);
+    if (options.sorted) residentsNames.sort();
     const obj = { [animal.name]: residentsNames };
     acc[currSpecie.location].push(obj);
     return acc;
   }, {});
 }
 
-function sortedTrue() {
-  return species.reduce((acc, currSpecie) => {
-    if (!acc[currSpecie.location]) acc[currSpecie.location] = [];
-    const animal = species.find((specie) => specie.name === currSpecie.name);
-    const residentsNames = animal.residents.map((resident) => resident.name).sort();
-    const obj = { [animal.name]: residentsNames };
-    acc[currSpecie.location].push(obj);
-    return acc;
-  }, {});
-}
+// function sortedTrue() {
+//   return species.reduce((acc, currSpecie) => {
+//     if (!acc[currSpecie.location]) acc[currSpecie.location] = [];
+//     const animal = species.find((specie) => specie.name === currSpecie.name);
+//     const residentsNames = animal.residents.map((resident) => resident.name).sort();
+//     const obj = { [animal.name]: residentsNames };
+//     acc[currSpecie.location].push(obj);
+//     return acc;
+//   }, {});
+// }
 
-function sexFilter(sex) {
+function sexFilter(options) {
   return species.reduce((acc, currSpecie) => {
     if (!acc[currSpecie.location]) acc[currSpecie.location] = [];
     const animal = species.find((specie) => specie.name === currSpecie.name);
     const residentsNames = animal.residents
-      .filter((resident) => resident.sex === sex)
+      .filter((resident) => resident.sex === options.sex)
       .map((element) => element.name);
+    if (options.sorted) residentsNames.sort();
     const obj = { [animal.name]: residentsNames };
     acc[currSpecie.location].push(obj);
     return acc;
@@ -47,11 +49,10 @@ function sexFilter(sex) {
 
 function getAnimalMap(options) {
   if (!options || !options.includeNames) return noParam();
-  if (options.sorted) return sortedTrue();
-  if (options.sex) return sexFilter(options.sex);
-
-  return includesNameTrue();
+  if (options.sex) return sexFilter(options);
+  return includesNameTrue(options);
 }
+
 console.log(getAnimalMap({ includeNames: true }));
 
 module.exports = getAnimalMap;
